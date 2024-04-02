dad8f55d36e1f4edf8890d7bbaad5b7564aed4e7
Eligijus Vitkauskas <eligijusvitkauskas@gmail.com>
2024-04-02 09:57:57 +0300

    Go format on save

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -191,7 +191,7 @@ return {
   {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && npm install',
+    build = 'cd app && yarn install',
     init = function()
       vim.g.mkdp_filetypes = { 'markdown' }
     end,
