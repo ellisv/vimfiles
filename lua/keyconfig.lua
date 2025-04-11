aa0ae929cf1bc654ac605fff4b078d06e9b29b04
Eligijus Vitkauskas <eligijusvitkauskas@gmail.com>
2025-04-11 20:37:50 +0300

    Add diagnostic keyconfig

--- lua/keyconfig.lua
+++ lua/keyconfig.lua
@@ -34,3 +34,7 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+vim.keymap.set('n', '<leader>dd', '<Cmd>lua vim.diagnostic.open_float(0, {scope = "line"})<CR>')
+vim.keymap.set('n', '<leader>dn', '<Cmd>lua vim.diagnostic.goto_next()<CR>')
+vim.keymap.set('n', '<leader>dp', '<Cmd>lua vim.diagnostic.goto_prev()<CR>')
