8921e25f841e64fea1b325661008331b96fb2349
Eligijus Vitkauskas <eligijus.vitkauskas@carvertical.com>
2025-05-13 07:12:20 +0300

    rm lualine plugin

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -15,22 +15,6 @@ return {
   },
 
   {
-    'nvim-lualine/lualine.nvim',
-    -- dependencies = { 'nvim-tree/nvim-web-devicons' }
-    config = function()
-      require('lualine').setup({
-        options = {
-          icons_enabled = false,
-          theme = 'molokai',
-          component_separators = { left = '', right = '' },
-          section_separators = { left = '', right = '' },
-        },
-        extensions = { 'neo-tree', 'nvim-dap-ui' },
-      })
-    end,
-  },
-
-  {
     'nvim-neo-tree/neo-tree.nvim',
     branch = 'v3.x',
     dependencies = {
