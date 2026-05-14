70cc8978c55fca982db63ef3391f9e920f382e85
Eligijus Vitkauskas <eligijusvitkauskas@gmail.com>
2026-05-14 16:05:24 +0300

    Disable copilot for .env files

--- lua/plugins/ide.lua
+++ lua/plugins/ide.lua
@@ -279,6 +279,13 @@ return {
         filetypes = {
           vimwiki = false,
           markdown = false,
+          sh = function ()
+            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+              -- disable for .env files
+              return false
+            end
+            return true
+          end,
           ["*"] = true,
         },
       })
