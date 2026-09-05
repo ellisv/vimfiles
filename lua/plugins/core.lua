46bc06715599e8b1b26aa0e06b7a05b3955bfd41
Eligijus Vitkauskas <eligijus.vitkauskas@carvertical.com>
2026-09-05 08:50:49 +0300

    Use lazygit for git log file view

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -198,7 +198,7 @@ return {
       vim.keymap.set("n", "<leader>gs", function() Snacks.picker.git_status() end, { desc = "Git Status" })
       vim.keymap.set("n", "<leader>gS", function() Snacks.picker.git_stash() end, { desc = "Git Stash" })
       vim.keymap.set("n", "<leader>gd", function() Snacks.picker.git_diff() end, { desc = "Git Diff (Hunks)" })
-      vim.keymap.set("n", "<leader>gf", function() Snacks.picker.git_log_file() end, { desc = "Git Log File" })
+      vim.keymap.set("n", "<leader>gf", function() Snacks.lazygit.log_file() end, { desc = "Git Log File" })
       -- gh
       vim.keymap.set("n", "<leader>gp", function() Snacks.picker.gh_pr() end, { desc = "GitHub Pull Requests (open)" })
       vim.keymap.set("n", "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, { desc = "GitHub Pull Requests (all)" })
